96ab9349ab58676ec6a1c7b1e6d8c1e1a92fcd00
Jasper <sankar@tendersoftware.in>
2021-11-02 15:01:04 +0530

    my first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/app.css">
+    <title>Start Laravel</title>
+</head>
+<body>
+<article>
+<h2><a href="welcome/my-first-post">my first post</a></h2>
+    <p>Lorem ipsum dolor sit amet consectetur 
+        adipisicing elit. Minima provident praesentium 
+        excepturi blanditiis porro animi voluptates 
+        totam exercitationem rerum ratione! 
+        Dolore ad aut debi
+        tis ab iste in provident, minima reprehenderit?
+        Lorem ipsum dolor sit, 
+        amet consectetur adipisicing elit. 
+        Praesentium obcaecati natus perspiciatis
+        voluptatibus mollitia, debitis, dignissimos
+        nulla aut incidunt 
+        deserunt beatae. Voluptatibus 
+        consequuntur ad nisi unde impedit eos, sit adipisci.
+    </p>
+</article>
+<article>
+<h2><a href="welcome/my-second-post">my Second post</a></h2>
+    <p>Lorem ipsum dolor sit amet consectetur 
+        adipisicing elit. Minima provident praesentium 
+        excepturi blanditiis porro animi voluptates 
+        totam exercitationem rerum ratione! 
+        Dolore ad aut debi
+        tis ab iste in provident, minima reprehenderit?
+        Lorem ipsum dolor sit, 
+        amet consectetur adipisicing elit. 
+        Praesentium obcaecati natus perspiciatis
+        voluptatibus mollitia, debitis, dignissimos
+        nulla aut incidunt 
+        deserunt beatae. Voluptatibus 
+        consequuntur ad nisi unde impedit eos, sit adipisci.
+    </p>
+
+</article>
+<article>
+<h2><a href="welcome/my-third-post">my third post</a></h2>
+    <p>Lorem ipsum dolor sit amet consectetur 
+        adipisicing elit. Minima provident praesentium 
+        excepturi blanditiis porro animi voluptates 
+        totam exercitationem rerum ratione! 
+        Dolore ad aut debi
+        tis ab iste in provident, minima reprehenderit?
+        Lorem ipsum dolor sit, 
+        amet consectetur adipisicing elit. 
+        Praesentium obcaecati natus perspiciatis
+        voluptatibus mollitia, debitis, dignissimos
+        nulla aut incidunt 
+        deserunt beatae. Voluptatibus 
+        consequuntur ad nisi unde impedit eos, sit adipisci.
+    </p>
+
+</article>
+
+</body>
+</html>
